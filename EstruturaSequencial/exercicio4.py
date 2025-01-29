342735c1991e3cbbc484622be7af7d0892e3bc7f
guimariz <--reset-author>
2025-01-29 20:53:21 -0300

    exercicio4 finalizado

---
+++ EstruturaSequencial/exercicio4.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota_bim = int(input("Informe 1a a nota: "))
+nota_bim2 = int(input("Informe 2a a nota: "))
+nota_bim3 = int(input("Informe 3a a nota: "))
+nota_bim4 = int(input("Informe 4a a nota: "))
+
+media = (nota_bim + nota_bim2 + nota_bim3 + nota_bim4) / 4
+
+print(media)
