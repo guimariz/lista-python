5bb74f8a155caf318e982a94c846ca1a8db75e8c
guimariz <--reset-author>
2025-01-29 20:47:58 -0300

    exercicio 3 finalizado

---
+++ EstruturaSequencial/exercicio2.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+# pedir um numero 
+# Mostrar mensagem com "numero"
+
+num_input = input("Informe um número: ")
+print(f'O número informado foi {num_input}')
---
+++ EstruturaSequencial/exercicio3.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça dois números e imprima a soma. 
+# entrada: 2 numeros
+# saidao: soma 2 numeros
+
+numero1 = int(input("informe o 1o numero: "))
+numero2 = int(input("informe o 2o numero: "))
+
+soma = numero1 + numero2  # concatenação -> colocar em sequencia
+print(soma)
+print(numero1 + numero2)
