286b9d16fa6645f19c0e5d302b27086ef03b9710
guimariz <--reset-author>
2025-01-29 21:56:57 -0300

    mais 1

---
+++ EstruturaDeRepetição/1.py
@@ -0,0 +1,14 @@
+variavel_string = "string"
+variavel_int = 10
+variavel_float = 10.5
+variavel_boolean = True
+
+
+print(variavel_string)
+print(type(variavel_string))
+print(variavel_int)
+print(type(variavel_int))
+print(variavel_float)
+print(type(variavel_float))
+print(variavel_boolean)
+print(type(variavel_boolean))
