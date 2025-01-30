5e555153954ab6a6b1d6e303f2bda31acb45ee4f
guimariz <--reset-author>
2025-01-29 22:01:31 -0300

    saidera

---
+++ EstruturaDeFuncoes/exercicio1.py
@@ -0,0 +1,8 @@
+# Faça um programa, com uma função que necessite de três argumentos, 
+# e que forneça a soma desses três argumentos. 
+
+def soma(arg1, arg2, arg3):
+    return arg1 + arg2 + arg3
+
+
+print(soma(1, 2, 3))
